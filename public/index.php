a9c9a8c9aa487a9ae9907cd7739576e95554005a
Radu Maier <ariademur@webserv.co>
2024-11-08 12:37:40 +0100

    Code style fixes.

--- public/index.php
+++ public/index.php
@@ -1,9 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
 use App\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
-return function (array $context) {
+return static function (array $context) {
+    assert(isset($context['APP_ENV']) && is_string($context['APP_ENV']));
+    assert(isset($context['APP_DEBUG']));
+
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
--- src/Kernel.php
+++ src/Kernel.php
@@ -1,11 +1,15 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
-class Kernel extends BaseKernel
+final class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    // Blank comment to prevent phpcs errors when class only contains trait usage and nothing else
 }
